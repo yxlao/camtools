18c98c14711bc60f8cf41aa680d4b297fa4dfadf
Yixing Lao <yixing.lao@gmail.com>
2022-03-13 00:01:01 -0800

    get camera center rays

--- geometry.py
+++ geometry.py
@@ -16,6 +16,7 @@ def get_camera_center_line(Ts, color=np.array([1, 0, 0])):
 
     return ls
 
+
 def get_camera_frame(T, size=0.1, color=[0, 0, 1]):
 
     R, t = T[:3, :3], T[:3, 3]
@@ -40,6 +41,7 @@ def get_camera_frame(T, size=0.1, color=[0, 0, 1]):
 
     return ls
 
+
 def get_camera_frames(Ts,
                       size=0.1,
                       color=[0, 0, 1],
@@ -51,11 +53,76 @@ def get_camera_frames(Ts,
         camera_frame = get_camera_frame(T, size=size, color=color)
         camera_frames += camera_frame
 
-    if len(Ts) > 1  and center_line:
+    if len(Ts) > 1 and center_line:
         center_line = get_camera_center_line(Ts, color=center_line_color)
         camera_frames += center_line
 
     return camera_frames
+
+
+def get_camera_center_ray(T, K, size=0.1, color=[0, 0, 1]):
+    """
+    T: 4x4
+    K: 3x3
+
+    Returns a linset of two points. The line starts the camera center and passes
+    through the center of the image.
+    """
+    if T.shape != (4, 4):
+        raise ValueError(f"T must has shape (4, 4), but got {T.shape}.")
+    if not np.allclose(T[3, :], [0, 0, 0, 1]):
+        raise ValueError(
+            f"T must has [0, 0, 0, 1] the bottom line, but got {T}.")
+    if K.shape != (3, 3):
+        raise ValueError(f"K must has shape (3, 3), but got {K.shape}.")
+
+    # Pick point at the center of the image
+    # Assumes that the camera offset is exactly at the center of the image.
+    col = K[0, 2]
+    row = K[1, 2]
+    points = np.array([
+        [col, row, 1],
+    ])
+
+    # Transform to camera space
+    points = (np.linalg.inv(K) @ points.T).T
+
+    # Normalize to have 1 distance
+    points = points / np.linalg.norm(points, axis=1, keepdims=True) * size
+
+    # Transform to world space
+    R, _ = convert.T_to_R_t(T)
+    C = convert.T_to_C(T)
+    points = (np.linalg.inv(R) @ points.T).T + C
+
+    # Create line set
+    points = np.vstack((C, points))
+    lines = np.array([
+        [0, 1],
+    ])
+    ls = o3d.geometry.LineSet()
+    ls.points = o3d.utility.Vector3dVector(points)
+    ls.lines = o3d.utility.Vector2iVector(lines)
+
+    return ls
+
+
+def get_camera_center_rays(Ts, Ks, size=0.1, color=[0, 0, 1]):
+    """
+    T: 4x4
+    K: 3x3
+
+    Returns a linset of two points. The line starts the camera center and passes
+    through the center of the image.
+    """
+    if len(Ts) != len(Ks):
+        raise ValueError(f"len(Ts) != len(Ks)")
+
+    camera_rays = o3d.geometry.LineSet()
+    for T, K in zip(Ts, Ks):
+        camera_rays += get_camera_center_ray(T, K, size=size, color=color)
+
+    return camera_rays
 
 
 def get_camera_ray_frame(T, K, size=0.1, color=[0, 0, 1]):
@@ -65,17 +132,14 @@ def get_camera_ray_frame(T, K, size=0.1, color=[0, 0, 1]):
     """
     T, K, color = np.asarray(T), np.asarray(K), np.asarray(color)
     if T.shape != (4, 4):
-        raise ValueError(
-            f"T must has shape (4, 4), but got {T.shape}.")
+        raise ValueError(f"T must has shape (4, 4), but got {T.shape}.")
     if not np.allclose(T[3, :], [0, 0, 0, 1]):
         raise ValueError(
             f"T must has [0, 0, 0, 1] the bottom line, but got {T}.")
     if K.shape != (3, 3):
-        raise ValueError(
-            f"K must has shape (3, 3), but got {K.shape}.")
+        raise ValueError(f"K must has shape (3, 3), but got {K.shape}.")
     if color.shape != (3,):
-        raise ValueError(
-            f"color must has shape (3,), but got {color.shape}.")
+        raise ValueError(f"color must has shape (3,), but got {color.shape}.")
 
     # Pick 4 corner points
     # Assumes that the camera offset is exactly at the center of the image.
@@ -102,7 +166,7 @@ def get_camera_ray_frame(T, K, size=0.1, color=[0, 0, 1]):
 
     # Create line set
     points = np.vstack((C, points))
-    lines = np.array(([
+    lines = np.array([
         [0, 1],
         [0, 2],
         [0, 3],
@@ -111,13 +175,14 @@ def get_camera_ray_frame(T, K, size=0.1, color=[0, 0, 1]):
         [2, 3],
         [3, 4],
         [4, 1],
-    ]))
+    ])
     ls = o3d.geometry.LineSet()
     ls.points = o3d.utility.Vector3dVector(points)
     ls.lines = o3d.utility.Vector2iVector(lines)
     ls.paint_uniform_color(color)
 
     return ls
+
 
 def get_camera_ray_frames(Ts,
                           Ks,
@@ -139,6 +204,7 @@ def get_camera_ray_frames(Ts,
         camera_frames += center_line
 
     return camera_frames
+
 
 def create_sphere_lineset(radius=1.0, resolution=10, color=[0, 0, 0]):
     sphere_mesh = o3d.geometry.TriangleMesh.create_sphere(radius=radius,
