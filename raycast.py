48d0526568209753709f0d9c718ddccd4a89ec2b
Yixing Lao <yixing.lao@gmail.com>
2022-05-28 06:36:07 -0700

    mesh to depth takes mesh as input

--- raycast.py
+++ raycast.py
@@ -4,13 +4,12 @@ import open3d as o3d
 from . import sanity
 
 
-def mesh_to_depth(vertices, triangles, K, T, height, width):
+def mesh_to_depth(mesh, K, T, height, width):
     """
     Cast mesh to depth image given camera parameters and image dimensions.
 
     Args:
-        vertices: (N, 3) array, 3D points in world coordinates.
-        triangles: (M, 3) array, indices of vertices.
+        mesh: Open3D mesh.
         K: (3, 3) array, camera intrinsic matrix.
         T: (4, 4) array, camera extrinsic matrix, [R | t] with [0, 0, 0, 1].
         height: int, image height.
@@ -24,19 +23,15 @@ def mesh_to_depth(vertices, triangles, K, T, height, width):
     need to perform ray casting of the same mesh multiple times, you should
     create the scene object manually to perform ray casting.
     """
-    sanity.assert_shape(vertices, (None, 3), name="vertices")
-    sanity.assert_shape(triangles, (None, 3), name="triangles")
     sanity.assert_K(K)
     sanity.assert_T(T)
 
-    assert triangles.dtype == np.int32 or triangles.dtype == np.int64
-
-    mesh = o3d.t.geometry.TriangleMesh(
-        vertex_positions=vertices.astype(np.float32),
-        triangle_indices=triangles,
+    t_mesh = o3d.t.geometry.TriangleMesh(
+        vertex_positions=np.asarray(mesh.vertices).astype(np.float32),
+        triangle_indices=np.asarray(mesh.triangles),
     )
     scene = o3d.t.geometry.RaycastingScene()
-    scene.add_triangles(mesh)
+    scene.add_triangles(t_mesh)
     rays = o3d.t.geometry.RaycastingScene.create_rays_pinhole(
         intrinsic_matrix=K,
         extrinsic_matrix=T,
