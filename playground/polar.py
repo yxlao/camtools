c4299294403580d89f3eba2f476e6893b14f68d1
Yixing Lao <yixing.lao@gmail.com>
2023-09-24 06:26:09 +0800

    simplify

--- playground/polar.py
+++ playground/polar.py
@@ -3,13 +3,12 @@ import numpy as np
 import open3d as o3d
 import itertools
 
-np.set_printoptions(precision=2, suppress=True)
-
 
 def euler_to_R(yaw, pitch, roll):
     """
-    Convert Euler angles to rotation matrix. Given a unit vector x, R @ x is x rotated by applying yaw, pitch, and roll consecutively.
-    Ref: https://en.wikipedia.org/wiki/Euler_angles
+    Convert Euler angles to rotation matrix. Given a unit vector x, R @ x is x
+    rotated by applying yaw, pitch, and roll consecutively. Ref:
+    https://en.wikipedia.org/wiki/Euler_angles
 
     Args:
         yaw (float): Rotation around the z-axis (from x-axis to y-axis).
@@ -55,16 +54,15 @@ def polar_to_T_towards_origin(radius, theta, phi):
     than pi/2.
 
     Args:
-        r (float): Radius, distance from the origin.
+        radius (float): Distance from the origin.
         theta (float): Inclination, angle w.r.t. positive polar (+z) axis.
             Range: [0, pi].
-        phi (float): Azimuth, rotation angle from the initial meridian (x-y) plane.
-            Range: [0, 2*pi].
+        phi (float): Azimuth, rotation angle from the initial meridian (x-y)
+            plane. Range: [0, 2*pi].
 
     Returns:
         T of shape (4, 4).
     """
-
     x = radius * np.sin(theta) * np.cos(phi)
     y = radius * np.sin(theta) * np.sin(phi)
     z = radius * np.cos(theta)
@@ -75,7 +73,7 @@ def polar_to_T_towards_origin(radius, theta, phi):
     # Rotate along z axis.
     phi_R = euler_to_R(phi + np.pi, 0, 0)
     # Rotate along y axis.
-    theta_R = euler_to_R(0, (np.pi / 2 - theta), 0)
+    theta_R = euler_to_R(0, np.pi / 2 - theta, 0)
 
     # Combine rotations, the order matters.
     R = phi_R @ theta_R @ init_R
@@ -88,6 +86,10 @@ def polar_to_T_towards_origin(radius, theta, phi):
 
 
 def main():
+    """
+    TODO: convert this to a unit test and check the center ray.
+    """
+
     # Create default camera intrinsics.
     K = np.array(
         [
