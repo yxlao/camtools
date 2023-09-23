c938d32c2f5945d751b27ab16e8d35264a3fcc91
Yixing Lao <yixing.lao@gmail.com>
2023-09-24 04:12:42 +0800

    circular camera

--- playground/polar.py
+++ playground/polar.py
@@ -83,12 +83,12 @@ def two_unit_vectors_to_R(v1, v2):
     return UU
 
 
-def polar_to_pose_lookat_origin_up_z(r, theta, phi):
+def polar_to_T_towards_origin(radius, theta, phi):
     """
-    Convert polar coordinates (ISO convention) to pose, where the cameras
-    looks at the origin from a distance r, and the camera up direction alines
-    with the z-axis (the angle between the up direction and the z-axis is
-    smaller than pi/2.
+    Convert polar coordinates (ISO convention) to T, where the cameras looks at
+    the origin from a distance (radius), and the camera up direction alines with
+    the z-axis (the angle between the up direction and the z-axis is smaller
+    than pi/2.
 
     Args:
         r (float): Radius, distance from the origin.
@@ -96,24 +96,20 @@ def polar_to_pose_lookat_origin_up_z(r, theta, phi):
         phi (float): Azimuth, angle from the z-axis.
 
     Returns:
-        Pose of shape (4, 4).
+        T of shape (4, 4).
     """
-    x = r * np.sin(theta) * np.cos(phi)
-    y = r * np.sin(theta) * np.sin(phi)
-    z = r * np.cos(theta)
+    x = radius * np.sin(theta) * np.cos(phi)
+    y = radius * np.sin(theta) * np.sin(phi)
+    z = radius * np.cos(theta)
 
-    yaw = np.pi / 2 - phi
-    pitch = np.pi / 2 - theta
-    roll = 0.0
-
-    R = euler_to_R(yaw, pitch, roll)
-    R = R.T
-
+    # Camera looking from the origin towards the Y-axis, up is Z-axis.
+    init_R = euler_to_R(0, 0, -np.pi / 2)
     pose = np.eye(4)
-    pose[:3, :3] = R
+    pose[:3, :3] = init_R
     pose[:3, 3] = [x, y, z]
+    T = ct.convert.pose_to_T(pose)
 
-    return pose
+    return T
 
 
 def main():
@@ -126,35 +122,23 @@ def main():
         ]
     )
 
-    x, y, z = 0.0, 0.0, 0.0
-    # Camera looking from the origin towards the Y-axis, up is Z-axis.
-    init_R = euler_to_R(0, 0, -np.pi / 2)
-    pose = np.eye(4)
-    pose[:3, :3] = init_R
-    pose[:3, 3] = [x, y, z]
-    T = ct.convert.pose_to_T(pose)
+    # Ks = [K]
+    # Ts = [T]
 
-    Ks = [K]
-    Ts = [T]
-
-    axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0)
-    camera = ct.camera.create_camera_ray_frames(Ks=Ks, Ts=Ts, size=2.0)
-    o3d.visualization.draw_geometries([camera, axes])
+    # axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0)
+    # camera = ct.camera.create_camera_ray_frames(Ks=Ks, Ts=Ts, size=2.0)
+    # o3d.visualization.draw_geometries([camera, axes])
 
     # # Create camera at x-y plane in a circle around the origin.
-    # num_cams = 12
-    # radius = 1.0
-    # thetas = np.linspace(0, 2 * np.pi, num_cams, endpoint=False)
+    num_cams = 12
+    radius = 1.0
+    thetas = np.linspace(0, 2 * np.pi, num_cams, endpoint=False)
+    Ts = [polar_to_T_towards_origin(radius, theta, 0) for theta in thetas]
+    Ks = [K for _ in range(num_cams)]
 
-    # poses = [polar_to_pose_towards_origin(radius, theta, 0) for theta in thetas]
-
-    # Ts = [ct.convert.pose_to_T(pose) for pose in poses]
-    # Ks = [K for _ in range(num_cams)]
-
-    # cameras = ct.camera.create_camera_ray_frames(Ks=Ks, Ts=Ts)
-    # axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0)
-
-    # o3d.visualization.draw_geometries([cameras, axes])
+    cameras = ct.camera.create_camera_ray_frames(Ks=Ks, Ts=Ts)
+    axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0)
+    o3d.visualization.draw_geometries([cameras, axes])
 
 
 if __name__ == "__main__":
