3c9f980a9845bf55d18aab0a1cf65411362e59e3
Yixing Lao <yixing.lao@gmail.com>
2023-09-24 04:54:08 +0800

    global cams

--- playground/polar.py
+++ playground/polar.py
@@ -1,8 +1,9 @@
 import camtools as ct
 import numpy as np
 import open3d as o3d
+import itertools
 
-np.set_printoptions(precision=4, suppress=True)
+np.set_printoptions(precision=2, suppress=True)
 
 
 def euler_to_R(yaw, pitch, roll):
@@ -102,12 +103,15 @@ def polar_to_T_towards_origin(radius, theta, phi):
     Returns:
         T of shape (4, 4).
     """
-    print(f"radius: {radius:.4f}, theta: {theta:.4f}, phi: {phi:.4f}")
+
     x = radius * np.sin(theta) * np.cos(phi)
     y = radius * np.sin(theta) * np.sin(phi)
     z = radius * np.cos(theta)
 
-    print(f"x: {x:.4f}, y: {y:.4f}, z: {z:.4f}")
+    print(
+        f"radius={radius:.2f}, theta={theta:.2f}, phi={phi:.2f}, "
+        f"x={x:.2f}, y={y:.2f}, z={z:.2f}"
+    )
 
     # Before    : look at +Z, up is -Y.
     # After init: look at +X, up is +Z.
@@ -145,14 +149,18 @@ def main():
     # o3d.visualization.draw_geometries([camera, axes])
 
     # # Create camera at x-y plane in a circle around the origin.
-    num_cams = 12
+    resolution = 12
     radius = 1.0
-    thetas = np.linspace(0, np.pi, num_cams, endpoint=False)
-    phis = np.linspace(0, 2 * np.pi, num_cams, endpoint=False)
-    Ts = [polar_to_T_towards_origin(radius, np.pi / 2, phi) for phi in phis]
-    Ks = [K for _ in range(num_cams)]
+    thetas = np.linspace(0, np.pi, resolution, endpoint=False)
+    thetas = thetas[2:-2]
+    phis = np.linspace(0, 2 * np.pi, resolution, endpoint=False)
 
-    cameras = ct.camera.create_camera_ray_frames(Ks=Ks, Ts=Ts)
+    thetas_phis = list(itertools.product(thetas, phis))
+
+    Ts = [polar_to_T_towards_origin(radius, theta, phi) for (theta, phi) in thetas_phis]
+    Ks = [K for _ in range(len(Ts))]
+
+    cameras = ct.camera.create_camera_ray_frames(Ks=Ks, Ts=Ts, center_line=False)
     axes = o3d.geometry.TriangleMesh.create_coordinate_frame(size=1.0)
     o3d.visualization.draw_geometries([cameras, axes])
 
