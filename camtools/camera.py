324ae3608112aac20e37310b30a5c3d66d005b88
Yixing Lao <yixing.lao@gmail.com>
2023-10-23 04:59:28 +0800

    rename to create_camera_frames and clean up

--- camtools/camera.py
+++ camtools/camera.py
@@ -19,126 +19,7 @@ def create_camera_center_line(Ts, color=np.array([1, 0, 0])):
     return ls
 
 
-def create_camera_frame(T, size=0.1, color=[0, 0, 1]):
-    R, t = T[:3, :3], T[:3, 3]
-
-    C0 = convert.R_t_to_C(R, t).ravel()
-    C1 = (
-        C0 + R.T.dot(np.array([[-size], [-size], [3 * size]], dtype=np.float32)).ravel()
-    )
-    C2 = (
-        C0 + R.T.dot(np.array([[-size], [+size], [3 * size]], dtype=np.float32)).ravel()
-    )
-    C3 = (
-        C0 + R.T.dot(np.array([[+size], [+size], [3 * size]], dtype=np.float32)).ravel()
-    )
-    C4 = (
-        C0 + R.T.dot(np.array([[+size], [-size], [3 * size]], dtype=np.float32)).ravel()
-    )
-
-    ls = o3d.geometry.LineSet()
-    points = np.array([C0, C1, C2, C3, C4])
-    lines = [[0, 1], [0, 2], [0, 3], [0, 4], [1, 2], [2, 3], [3, 4], [4, 1]]
-    colors = np.tile(color, (len(lines), 1))
-    ls.points = o3d.utility.Vector3dVector(points)
-    ls.lines = o3d.utility.Vector2iVector(lines)
-    ls.colors = o3d.utility.Vector3dVector(colors)
-
-    return ls
-
-
-def create_camera_frames(
-    Ts,
-    size=0.1,
-    color=[0, 0, 1],
-    start_color=[0, 1, 0],
-    end_color=[1, 0, 0],
-    center_line=True,
-    center_line_color=[1, 0, 0],
-):
-    camera_frames = o3d.geometry.LineSet()
-    for index, T in enumerate(Ts):
-        if index == 0:
-            frame_color = start_color
-        elif index == len(Ts) - 1:
-            frame_color = end_color
-        else:
-            frame_color = color
-        camera_frame = create_camera_frame(T, size=size, color=frame_color)
-        camera_frames += camera_frame
-
-    if len(Ts) > 1 and center_line:
-        center_line = create_camera_center_line(Ts, color=center_line_color)
-        camera_frames += center_line
-
-    return camera_frames
-
-
-def create_camera_center_ray(K, T, size=0.1, color=[0, 0, 1]):
-    """
-    K: 3x3
-    T: 4x4
-
-    Returns a linset of two points. The line starts the camera center and passes
-    through the center of the image.
-    """
-    sanity.assert_T(T)
-    sanity.assert_K(K)
-
-    # Pick point at the center of the image
-    # Assumes that the camera offset is exactly at the center of the image.
-    col = K[0, 2]
-    row = K[1, 2]
-    points = np.array(
-        [
-            [col, row, 1],
-        ]
-    )
-
-    # Transform to camera space
-    points = (np.linalg.inv(K) @ points.T).T
-
-    # Normalize to have 1 distance
-    points = points / np.linalg.norm(points, axis=1, keepdims=True) * size
-
-    # Transform to world space
-    R, _ = convert.T_to_R_t(T)
-    C = convert.T_to_C(T)
-    points = (np.linalg.inv(R) @ points.T).T + C
-
-    # Create line set
-    points = np.vstack((C, points))
-    lines = np.array(
-        [
-            [0, 1],
-        ]
-    )
-    ls = o3d.geometry.LineSet()
-    ls.points = o3d.utility.Vector3dVector(points)
-    ls.lines = o3d.utility.Vector2iVector(lines)
-
-    return ls
-
-
-def create_camera_center_rays(Ks, Ts, size=0.1, color=[0, 0, 1]):
-    """
-    K: 3x3
-    T: 4x4
-
-    Returns a linset of two points. The line starts the camera center and passes
-    through the center of the image.
-    """
-    if len(Ts) != len(Ks):
-        raise ValueError(f"len(Ts) != len(Ks)")
-
-    camera_rays = o3d.geometry.LineSet()
-    for T, K in zip(Ts, Ks):
-        camera_rays += create_camera_center_ray(T, K, size=size, color=color)
-
-    return camera_rays
-
-
-def _create_camera_ray_frame(K, T, image_wh, size, color, disable_up_triangle):
+def _create_camera_frame(K, T, image_wh, size, color, disable_up_triangle):
     """
     K: (3, 3)
     T: (4, 4)
@@ -257,15 +138,15 @@ def _wrap_dim(dim: int, max_dim: int, inclusive: bool = False) -> int:
     return dim
 
 
-def create_camera_ray_frames(
+def create_camera_frames(
     Ks,
     Ts,
     image_whs=None,
     size=0.1,
-    color=[0, 0, 1],
+    color=(0, 0, 1),
     highlight_color_map=None,
     center_line=True,
-    center_line_color=[1, 0, 0],
+    center_line_color=(1, 0, 0),
     disable_up_triangle=False,
 ):
     """
@@ -338,7 +219,7 @@ def create_camera_ray_frames(
             frame_color = highlight_color_map[index]
         else:
             frame_color = color
-        camera_frame = _create_camera_ray_frame(
+        camera_frame = _create_camera_frame(
             K,
             T,
             image_wh=image_wh,
@@ -356,3 +237,29 @@ def create_camera_ray_frames(
         ls += center_line
 
     return ls
+
+
+def create_camera_frames_with_Ts(
+    Ts,
+    image_whs=None,
+    size=0.1,
+    color=(0, 0, 1),
+    highlight_color_map=None,
+    center_line=True,
+    center_line_color=(1, 0, 0),
+    disable_up_triangle=False,
+):
+    """
+    Returns ct.camera.create_camera_frames(Ks=None, Ts, ...).
+    """
+    return create_camera_frames(
+        Ks=None,
+        Ts=Ts,
+        image_whs=image_whs,
+        size=size,
+        color=color,
+        highlight_color_map=highlight_color_map,
+        center_line=center_line,
+        center_line_color=center_line_color,
+        disable_up_triangle=disable_up_triangle,
+    )
