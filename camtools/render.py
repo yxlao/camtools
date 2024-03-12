8560bda92a72df767be94e00d7f37c7d1cf2a8f8
Yixing Lao <yixing.lao@gmail.com>
2024-03-13 00:28:42 +0800

    transforming geometries are equivalent to transforming cameras

--- camtools/render.py
+++ camtools/render.py
@@ -1,9 +1,10 @@
+import copy
 from typing import List, Tuple
 
 import numpy as np
 import open3d as o3d
 
-from . import sanity
+from . import convert, sanity
 
 
 def render_geometries(
@@ -95,6 +96,57 @@ def render_geometries(
     return im_buffer
 
 
+def render_geometries_with_reset_light(
+    geometries: List[o3d.geometry.Geometry3D],
+    K: np.ndarray = None,
+    T: np.ndarray = None,
+    height: int = 720,
+    width: int = 1280,
+    visible: bool = False,
+    point_size: float = 1.0,
+):
+    """
+    Renders geometries with a transformed view by applying the inverse
+    transformation to the geometries instead of setting a new camera view.
+
+    Args:
+        geometries: List of Open3D geometry objects to render.
+        K: Camera intrinsic matrix. If None, Open3D's default is used.
+        T: The target camera extrinsic matrix to simulate.
+        height: The height of the rendered image.
+        width: The width of the rendered image.
+        visible: Whether the Open3D window is visible during rendering.
+        point_size: Point size for point cloud objects.
+    """
+    K_default, T_default = get_render_K_T(
+        geometries,
+        K=K,
+        T=None,
+        height=height,
+        width=width,
+    )
+
+    # Compute T compensation, where:
+    # - render(geometries, T_new)
+    # - render(geometries.transform(T_comp), T_default)
+    # are equivalent.
+    T_comp = np.linalg.inv(T_default) @ T
+
+    geometries_transformed = [
+        copy.deepcopy(geometry).transform(T_comp) for geometry in geometries
+    ]
+
+    return render_geometries(
+        geometries_transformed,
+        K=K,
+        T=T_default,
+        height=height,
+        width=width,
+        visible=visible,
+        point_size=point_size,
+    )
+
+
 def get_render_view_status_str(
     geometries: List[o3d.geometry.Geometry3D],
     K: np.ndarray = None,
@@ -155,12 +207,12 @@ def get_render_view_status_str(
             cy=K[1, 2],
         )
         o3d_extrinsic = T
-        o3d_camera = o3d.camera.PinholeCameraParameters()
-        o3d_camera.intrinsic = o3d_intrinsic
-        o3d_camera.extrinsic = o3d_extrinsic
+        cam_params = o3d.camera.PinholeCameraParameters()
+        cam_params.intrinsic = o3d_intrinsic
+        cam_params.extrinsic = o3d_extrinsic
         ctr = vis.get_view_control()
         ctr.convert_from_pinhole_camera_parameters(
-            o3d_camera,
+            cam_params,
             allow_arbitrary=True,
         )
 
@@ -174,6 +226,8 @@ def get_render_view_status_str(
 
 def get_render_K_T(
     geometries: List[o3d.geometry.Geometry3D],
+    K: np.ndarray = None,
+    T: np.ndarray = None,
     view_status_str: str = None,
     height: int = 720,
     width: int = 1280,
@@ -183,9 +237,14 @@ def get_render_K_T(
 
     Args:
         geometries: List of Open3D geometries.
+        K: (3, 3) np.ndarray camera intrinsic. If None, use Open3D's camera
+            inferred from the geometries. You may optionally provide K or T.
+        T: (4, 4) np.ndarray camera extrinsic. If None, use Open3D's camera
+            inferred from the geometries. You may optionally provide K or T.
         view_status_str: Optional. The json string returned by
             o3d.visualization.Visualizer.get_view_status(), containing
-            the viewing camera parameters.
+            the viewing camera parameters. view_status_str takes precedence
+            over K and T.
         height: int, image height.
         width: int, image width.
 
@@ -213,6 +272,30 @@ def get_render_K_T(
     vis.update_renderer()
     ctr = vis.get_view_control()
     cam_params = ctr.convert_to_pinhole_camera_parameters()
+
+    # Use K or T if provided, but view_status_str takes precedence.
+    if not view_status_str:
+        if K is not None:
+            o3d_intrinsic = o3d.camera.PinholeCameraIntrinsic(
+                width=width,
+                height=height,
+                fx=K[0, 0],
+                fy=K[1, 1],
+                cx=K[0, 2],
+                cy=K[1, 2],
+            )
+            cam_params.intrinsic = o3d_intrinsic
+        if T is not None:
+            cam_params.extrinsic = T
+        ctr.convert_from_pinhole_camera_parameters(
+            cam_params,
+            allow_arbitrary=True,
+        )
+
+        vis.poll_events()
+        vis.update_renderer()
+        ctr = vis.get_view_control()
+        cam_params = ctr.convert_to_pinhole_camera_parameters()
 
     K = np.copy(np.array(cam_params.intrinsic.intrinsic_matrix))
     T = np.copy(np.array(cam_params.extrinsic))
