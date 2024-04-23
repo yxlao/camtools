7e343a369b12e0eee3c7980ab67fdcbf49a086e4
Yixing Lao <yixing.lao@gmail.com>
2024-04-23 13:41:46 +0800

    new: transform a single point

--- camtools/transform.py
+++ camtools/transform.py
@@ -27,3 +27,22 @@ def transform_points(points, transform_mat):
     points_transformed = convert.from_homo(points_transformed)
 
     return points_transformed
+
+
+def transform_point(point, transform_mat):
+    """
+    Transform a single point by a 4x4 matrix via homogenous coordinates projection.
+
+    Args:
+        point: (3,) array.
+        mat: (4, 4) array, the transformation matrix.
+
+    Returns:
+        (3,) array, the transformed point.
+    """
+    sanity.assert_shape_3(point, name="point")
+    sanity.assert_shape_4x4(transform_mat, name="mat")
+
+    point_transformed = transform_points(point[None], transform_mat)[0]
+
+    return point_transformed
