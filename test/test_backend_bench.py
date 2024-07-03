a629f500c987dea0adbbf7bf7bfb0ecbefc8b357
Yixing Lao <yixing.lao@gmail.com>
2024-07-04 07:48:22 +0800

    add benchmark

--- test/test_backend_bench.py
+++ test/test_backend_bench.py
@@ -5,6 +5,8 @@ from jaxtyping import Float
 
 from camtools.backend import Tensor, ivy, is_torch_available, torch
 
+_workload_repeat = 100
+
 
 def test_concat_numpy_native(benchmark):
 
@@ -12,6 +14,8 @@ def test_concat_numpy_native(benchmark):
         x: Float[Tensor, "..."],
         y: Float[Tensor, "..."],
     ):
+        for _ in range(_workload_repeat):
+            np.concatenate([x, y], axis=0)
         return np.concatenate([x, y], axis=0)
 
     x = np.array([1.0, 2.0, 3.0])
@@ -22,13 +26,35 @@ def test_concat_numpy_native(benchmark):
 
 
 @pytest.mark.skipif(not is_torch_available(), reason="Torch is not available")
-def test_concat_torch_to_numpy(benchmark):
+def test_concat_torch_to_numpy_manual(benchmark):
+
+    def concat(
+        x: Float[Tensor, "..."],
+        y: Float[Tensor, "..."],
+    ):
+        x = x.cpu().numpy()
+        y = y.cpu().numpy()
+        for _ in range(_workload_repeat):
+            np.concatenate([x, y], axis=0)
+        return np.concatenate([x, y], axis=0)
+
+    x = torch.tensor([1.0, 2.0, 3.0])
+    y = torch.tensor([4.0, 5.0, 6.0])
+    result = benchmark(concat, x, y)
+    assert isinstance(result, np.ndarray)
+    assert np.array_equal(result, np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]))
+
+
+@pytest.mark.skipif(not is_torch_available(), reason="Torch is not available")
+def test_concat_torch_to_numpy_auto(benchmark):
 
     @ct.backend.tensor_numpy_backend
     def concat(
         x: Float[Tensor, "..."],
         y: Float[Tensor, "..."],
     ):
+        for _ in range(_workload_repeat):
+            np.concatenate([x, y], axis=0)
         return np.concatenate([x, y], axis=0)
 
     x = torch.tensor([1.0, 2.0, 3.0])
