1319f3ca4414b3f0953ff1ac36aa43c2f136b97a
Yixing Lao <yixing.lao@gmail.com>
2024-07-08 23:30:45 +0800

    add parameters

--- playground/demo.py
+++ playground/demo.py
@@ -6,21 +6,24 @@ from camtools.backend import Tensor, torch
 
 
 @ct.backend.tensor_to_numpy_backend
-def _dot(
-    x: Float[Tensor, "..."],
-    y: Float[Tensor, "..."],
+def _sum(
+    x: Float[Tensor, "3"],
+    y: Float[Tensor, "3"] = torch.tensor([2.0, 2.0, 2.0]),
+    z: Float[Tensor, "3"] = torch.tensor([3.0, 3.0, 3.0]),
 ):
     assert isinstance(x, np.ndarray)
     assert isinstance(y, np.ndarray)
-    return np.dot(x, y)
+    assert isinstance(z, np.ndarray)
+    return x + y + z
 
 
 def main():
-    x = torch.tensor([1.0, 2.0, 3.0])
-    y = torch.tensor([4.0, 5.0, 6.0])
-    result = _dot(x, y)
-    assert result == 32.0
-    print("Success!")
+    x = torch.tensor([1.0, 1.0, 1.0])
+    y = torch.tensor([5.0, 5.0, 5.0])
+    result = _sum(x, y=y)
+    expected_result = np.array([9.0, 9.0, 9.0])
+    assert np.allclose(result, expected_result)
+    print("All tests passed!")
 
 
 if __name__ == "__main__":
