455ed58a9879dc4521b39d2ba24845d86a8b905f
Yixing Lao <yixing.lao@gmail.com>
2024-07-08 15:54:05 +0800

    simplify test

--- test/test_backend_bench.py
+++ test/test_backend_bench.py
@@ -8,23 +8,6 @@ from camtools.backend import Tensor, ivy, is_torch_available, torch
 _workload_repeat = 100
 
 
-def test_concat_numpy_native(benchmark):
-
-    def concat(
-        x: Float[Tensor, "..."],
-        y: Float[Tensor, "..."],
-    ):
-        for _ in range(_workload_repeat):
-            np.concatenate([x, y], axis=0)
-        return np.concatenate([x, y], axis=0)
-
-    x = np.array([1.0, 2.0, 3.0])
-    y = np.array([4.0, 5.0, 6.0])
-    result = benchmark(concat, x, y)
-    assert isinstance(result, np.ndarray)
-    assert np.array_equal(result, np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]))
-
-
 @pytest.mark.skipif(not is_torch_available(), reason="Torch is not available")
 def test_concat_torch_to_numpy_manual(benchmark):
 
