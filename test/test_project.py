c6f25897b0c378711c0e7bb2763f49e09d23c61c
Yixing Lao <yixing.lao@gmail.com>
2025-01-04 02:18:15 +0800

    test(test_project.py): adjust camera position and orientation parameters for more realistic testing scenarios refactor(test_project.py): reduce frustum size to 0.2 for better visualization and testing accuracy

--- test/test_project.py
+++ test/test_project.py
@@ -11,12 +11,16 @@ def test_points_to_pixels():
             [0, 0, 1],
         ]
     )
+    # Place camera 2 units away from origin, looking towards origin
+    # with a slight rotation for testing
     R = ct.convert.roll_pitch_yaw_to_R(
         roll=np.pi / 6,
-        pitch=np.pi / 4,
+        pitch=-np.pi / 4,
         yaw=np.pi / 3,
     )
-    t = np.array([1, 2, 3])
+    # Position camera closer to origin
+    t = np.array([0.1, 0.2, 0.3])
+
     T = ct.convert.R_t_to_T(R, t)
 
     # Plot cameras
@@ -27,7 +31,7 @@ def test_points_to_pixels():
     frustum = ct.camera.create_camera_frustums(
         Ks=[K],
         Ts=[T],
-        size=0.5,
+        size=0.2,
         up_triangle=True,
     )
 
